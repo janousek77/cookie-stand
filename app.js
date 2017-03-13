3891388708bf7e70181ee10157e6593db85addbc
janousek77 <janousek77@gmail.com>
2017-03-13 16:50:03 -0700

    added rough webpage javascript

--- app.js
+++ app.js
@@ -15,13 +15,22 @@ var firstAndPike = {
     var total = 0;
     for (var i = 0; i < hours.length; i++) {
       this.hourlySales.push(Math.ceil(this.randomCustPerHour() * this.avgCookies));
-      console.log(this.hourlySales);
       total = total + this.hourlySales[i];
       console.log(total);
     }
     this.hourlySales.push(total);
   }
 };
+
+var body = document.getElementsByTagName('body');
+var firstList = document.createElement('ul');
+for (i = 0; i < hours.length; i++){
+  var firstListItem = document.createElement('li');
+  firstListItem.textContent = firstAndPike.hourlySales[i];
+  firstList.appendChild(firstListItem);
+  console.log(ul);
+};
+firstList.appendChild(body);
 
 var seatac = {
   minCust: 3,
