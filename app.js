b76b47a6c5f4e899fb586238e8ebed9604519714
janousek77 <janousek77@gmail.com>
2017-03-13 14:17:49 -0700

    made progress on problem

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
 'strict';
 
+var hours = ['8am', '9am', '10am' , '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', 'total'];
+
 var firstAndPike = {
   minCust: 23,
   maxCust: 65,
   avgCookies: 6.3,
-  totalSales: Math.floor(Math.random() * (maxCust - minCust)) + minCust,
-  cookieSales: function(){
-    return totalSales * avgCookies
+  hourlySales: [],
+  sales: function() {
+    var total = 0;
+    for (var i = 0; i <= hours.length - 1; i++) {
+    totalSales.push(Math.ceil(Math.floor(Math.random() * (maxCust - minCust)) + minCust * (avgCookies)));
+    total = total + hours[i];
+    hours[i+1] = total
+    }
+  sales.push(total);
   }
-  console.log(return;)
 };
 
 var seatac = {
   minCust: 3,
   maxCust: 24,
   avgCookies: 1.2,
-  totalSales: Math.floor(Math.random() * 12) + 1,
+  totalSales: Math.floor(Math.random() * (maxCust - minCust)) + minCust * (avgCookies),
   cookieSales: function(){
   }
 };
@@ -24,7 +31,7 @@ var seattleCenter = {
   minCust: 11,
   maxCust: 38,
   avgCookies: 3.7,
-  totalSales: Math.floor(Math.random() * 12) + 1,
+  totalSales: Math.floor(Math.random() * (maxCust - minCust)) + minCust * (avgCookies),
   cookieSales: function(){
   }
 };
@@ -33,7 +40,7 @@ var capHill = {
   minCust: 20,
   maxCust: 38,
   avgCookies: 2.3,
-  totalSales: Math.floor(Math.random() * 12) + 1,
+  totalSales: Math.floor(Math.random() * (maxCust - minCust)) + minCust * (avgCookies),
   cookieSales: function(){
   }
 };
@@ -42,13 +49,10 @@ var alki = {
   minCust: 2,
   maxCust: 16,
   avgCookies: 4.6,
-  totalSales: Math.floor(Math.random() * 12) + 1,
   cookieSales: function(){
   }
 };
 
-var hours = ['8am', '9am', '10am' , '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
-for (var i = 0; i < hours.length; i++) {
   // do some operationas
-  // this operation should push new cookie sales for every hour
+  hours[i] = totalSales
 }
