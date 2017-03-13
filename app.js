88b4f48282b5471ef7e0a843fc1995ff91e0dea1
janousek77 <janousek77@gmail.com>
2017-03-13 15:58:56 -0700

    finished functions for all stores

--- app.js
+++ app.js
@@ -7,14 +7,19 @@ var firstAndPike = {
   maxCust: 65,
   avgCookies: 6.3,
   hourlySales: [],
+
+  randomCustPerHour: function(){
+    return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+  },
   sales: function() {
     var total = 0;
-    for (var i = 0; i <= hours.length - 1; i++) {
-    totalSales.push(Math.ceil(Math.floor(Math.random() * (maxCust - minCust)) + minCust * (avgCookies)));
-    total = total + hours[i];
-    hours[i+1] = total
+    for (var i = 0; i < hours.length; i++) {
+      this.hourlySales.push(Math.ceil(this.randomCustPerHour() * this.avgCookies));
+      console.log(this.hourlySales);
+      total = total + this.hourlySales[i];
+      console.log(total);
     }
-  sales.push(total);
+    this.hourlySales.push(total);
   }
 };
 
@@ -22,8 +27,20 @@ var seatac = {
   minCust: 3,
   maxCust: 24,
   avgCookies: 1.2,
-  totalSales: Math.floor(Math.random() * (maxCust - minCust)) + minCust * (avgCookies),
-  cookieSales: function(){
+  hourlySales: [],
+
+  randomCustPerHour: function(){
+    return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+  },
+  sales: function() {
+    var total = 0;
+    for (var i = 0; i < hours.length; i++) {
+      this.hourlySales.push(Math.ceil(this.randomCustPerHour() * this.avgCookies));
+      console.log(this.hourlySales);
+      total = total + this.hourlySales[i];
+      console.log(total);
+    }
+    this.hourlySales.push(total);
   }
 };
 
@@ -31,17 +48,20 @@ var seattleCenter = {
   minCust: 11,
   maxCust: 38,
   avgCookies: 3.7,
-  totalSales: Math.floor(Math.random() * (maxCust - minCust)) + minCust * (avgCookies),
-  cookieSales: function(){
-  }
-};
+  hourlySales: [],
 
-var capHill = {
-  minCust: 20,
-  maxCust: 38,
-  avgCookies: 2.3,
-  totalSales: Math.floor(Math.random() * (maxCust - minCust)) + minCust * (avgCookies),
-  cookieSales: function(){
+  randomCustPerHour: function(){
+    return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+  },
+  sales: function() {
+    var total = 0;
+    for (var i = 0; i < hours.length; i++) {
+      this.hourlySales.push(Math.ceil(this.randomCustPerHour() * this.avgCookies));
+      console.log(this.hourlySales);
+      total = total + this.hourlySales[i];
+      console.log(total);
+    }
+    this.hourlySales.push(total);
   }
 };
 
@@ -49,10 +69,19 @@ var alki = {
   minCust: 2,
   maxCust: 16,
   avgCookies: 4.6,
-  cookieSales: function(){
+  hourlySales: [],
+
+  randomCustPerHour: function(){
+    return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+  },
+  sales: function() {
+    var total = 0;
+    for (var i = 0; i < hours.length; i++) {
+      this.hourlySales.push(Math.ceil(this.randomCustPerHour() * this.avgCookies));
+      console.log(this.hourlySales);
+      total = total + this.hourlySales[i];
+      console.log(total);
+    }
+    this.hourlySales.push(total);
   }
 };
-
-  // do some operationas
-  hours[i] = totalSales
-}
