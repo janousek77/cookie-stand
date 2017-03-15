ffb764a763596e7f63f7bff371e131def7a61e6a
janousek77 <janousek77@gmail.com>
2017-03-15 09:54:03 -0700

    fixed table body

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 'strict';
 
-var hours = ['Stores','8am', '9am', '10am' , '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', 'totals'];
+var hours = ['Stores','8am', '9am', '10am' , '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', 'Totals'];
 var body = document.getElementsByTagName('body')[0];
 
-var first = new store('1st and Pike', 23, 65, 6.3);
-var seatac = new store('Seatac Airport', 3, 24, 1.2);
-var seattleCenter = new store('Seattle Center', 11, 38, 3.7);
-var capHill = new store('Capitol Hill', 20, 38, 2.3);
-var alki = new store('Alki', 2, 16, 4.6);
+var first = new Store('1st and Pike', 23, 65, 6.3);
+var seatac = new Store('Seatac Airport', 3, 24, 1.2);
+var seattleCenter = new Store('Seattle Center', 11, 38, 3.7);
+var capHill = new Store('Capitol Hill', 20, 38, 2.3);
+var alki = new Store('Alki', 2, 16, 4.6);
 
-function store(sname, min, max, aveCookies) {
+function Store(sname, min, max, aveCookies) {
   this.name = sname;
   this.minCust = min;
   this.maxCust = max;
@@ -26,20 +26,18 @@ function store(sname, min, max, aveCookies) {
       this.total += cookiesPerHour;
     }
     this.hourlySales.push(this.total);
-    console.log(this.total);
   };
   this.rows = function(){
     this.cookiesSold();
     var table = document.getElementsByTagName('table')[0];
-    var tbody = document.createElement('tbody');
-    table.appendChild(tbody);
+    var tbody = document.getElementsByTagName('tbody')[0];
     var tableRow = document.createElement('tr');
     tbody.appendChild(tableRow);
-    var tData = document.createElement('td');
-    tData.innerText = this.name;
-    tableRow.appendChild(tData);
+    var tdName = document.createElement('td');
+    tdName.innerText = this.name;
+    tableRow.appendChild(tdName);
     for (var i = 0; i < hours.length - 1; i++){
-      var data = document.createElement('td');;
+      var data = document.createElement('td');
       data.innerText = this.hourlySales[i];
       tableRow.appendChild(data);
     }
@@ -51,6 +49,8 @@ function table(){
   body.appendChild(table);
   var thead = document.createElement('thead');
   table.appendChild(thead);
+  var tbody = document.createElement('tbody');
+  table.appendChild(tbody);
   var tableRow = document.createElement('tr');
   thead.appendChild(tableRow);
   for (var i = 0; i < hours.length; i++){
